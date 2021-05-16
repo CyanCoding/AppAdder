4d2d592b679f36477bf16b91d986852181d2f373
Camden <CyanCoding@users.noreply.github.com>
2021-05-16 17:13:36 -0400

    Create main window
    Created the main window with button functionality.

--- AppAdder/mainwindow.cpp
+++ AppAdder/mainwindow.cpp
@@ -1,15 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QFileDialog>
+#include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    // Buttons
+    ui->locateIcon->setText("..!");
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+
+void MainWindow::on_locateIcon_clicked() {
+    // Get the user's home directory, like '/home/jason'
+    struct passwd *pw = getpwuid(getuid());
+    const char *homedir = pw->pw_dir;
+
+    QString selectedIcon = QFileDialog::getOpenFileName(this, "Choose Icon", homedir, "PNG Image (*.png)");
+
+    // Sets the icon box to the icon's file location
+    ui->iconBox->setText(selectedIcon);
 }
 
---
+++ build-AppAdder-Desktop_Qt_6_1_0_GCC_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,79 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.1.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QLineEdit *iconBox;
+    QLabel *label;
+    QPushButton *locateIcon;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        iconBox = new QLineEdit(centralwidget);
+        iconBox->setObjectName(QString::fromUtf8("iconBox"));
+        iconBox->setGeometry(QRect(90, 180, 131, 21));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(90, 150, 41, 21));
+        locateIcon = new QPushButton(centralwidget);
+        locateIcon->setObjectName(QString::fromUtf8("locateIcon"));
+        locateIcon->setGeometry(QRect(230, 180, 41, 21));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "AppAdder", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Icon:", nullptr));
+        locateIcon->setText(QCoreApplication::translate("MainWindow", "...", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
